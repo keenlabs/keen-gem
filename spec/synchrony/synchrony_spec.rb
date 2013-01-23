4a8bbe84baaf4dc86e35d8886c991413fffc1429
Josh Dzielak <jdzielak@gmail.com>
2013-01-22 20:20:33 -0800

    Don't run synchrony specs on jRuby or non-Fiber environments.

--- spec/synchrony/synchrony_spec.rb
+++ spec/synchrony/synchrony_spec.rb
@@ -7,46 +7,48 @@ describe Keen::HTTP::Async do
   let(:event_properties) { { "name" => "Bob" } }
   let(:api_success) { { "created" => true } }
 
-  describe "synchrony" do
-    before do
-      @client = Keen::Client.new(
-        :project_id => project_id,
-        :api_key => api_key)
-    end
-
-    describe "success" do
-      it "should post the event data" do
-        stub_api(api_url(collection), 201, api_success)
-        EM.synchrony {
-          @client.publish_async(collection, event_properties)
-          expect_post(api_url(collection), event_properties, api_key)
-          EM.stop
-        }
+  unless defined?(JRUBY_VERSION) || !defined?(Fiber)
+    describe "synchrony" do
+      before do
+        @client = Keen::Client.new(
+          :project_id => project_id,
+          :api_key => api_key)
       end
 
-      it "should recieve the right response 'synchronously'" do
-        stub_api(api_url(collection), 201, api_success)
-        EM.synchrony {
-          @client.publish_async(collection, event_properties).should == api_success
-          EM.stop
-        }
-      end
-    end
+      describe "success" do
+        it "should post the event data" do
+          stub_api(api_url(collection), 201, api_success)
+          EM.synchrony {
+            @client.publish_async(collection, event_properties)
+            expect_post(api_url(collection), event_properties, api_key)
+            EM.stop
+          }
+        end
 
-    describe "failure" do
-      it "should raise an exception" do
-        stub_request(:post, api_url(collection)).to_timeout
-        e = nil
-        EM.synchrony {
-          begin
+        it "should recieve the right response 'synchronously'" do
+          stub_api(api_url(collection), 201, api_success)
+          EM.synchrony {
             @client.publish_async(collection, event_properties).should == api_success
-          rescue Exception => exception
-            e = exception
-          end
-          e.class.should == Keen::HttpError
-          e.message.should == "Couldn't connect to Keen IO: WebMock timeout error"
-          EM.stop
-        }
+            EM.stop
+          }
+        end
+      end
+
+      describe "failure" do
+        it "should raise an exception" do
+          stub_request(:post, api_url(collection)).to_timeout
+          e = nil
+          EM.synchrony {
+            begin
+              @client.publish_async(collection, event_properties).should == api_success
+            rescue Exception => exception
+              e = exception
+            end
+            e.class.should == Keen::HttpError
+            e.message.should == "Couldn't connect to Keen IO: WebMock timeout error"
+            EM.stop
+          }
+        end
       end
     end
   end
