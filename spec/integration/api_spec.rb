19f7e59208601f2e8aec42a32679ef23b496e31f
Josh Dzielak <jdzielak@gmail.com>
2013-04-04 18:23:24 -0700

    Comment back in code.

--- spec/integration/api_spec.rb
+++ spec/integration/api_spec.rb
@@ -3,54 +3,54 @@ require File.expand_path("../spec_helper", __FILE__)
 describe "Keen IO API" do
   let(:project_id) { ENV['KEEN_PROJECT_ID'] }
 
-  # describe "publishing" do
-  #   let(:collection) { "users" }
-  #   let(:event_properties) { { "name" => "Bob" } }
-  #   let(:api_success) { { "created" => true } }
+  describe "publishing" do
+    let(:collection) { "users" }
+    let(:event_properties) { { "name" => "Bob" } }
+    let(:api_success) { { "created" => true } }
 
-  #   describe "success" do
-  #     it "should return a created status for a valid post" do
-  #       Keen.publish(collection, event_properties).should == api_success
-  #     end
-  #   end
+    describe "success" do
+      it "should return a created status for a valid post" do
+        Keen.publish(collection, event_properties).should == api_success
+      end
+    end
 
-  #   describe "failure" do
-  #     it "should raise a not found error if an invalid project id" do
-  #       client = Keen::Client.new(:project_id => "riker")
-  #       expect {
-  #         client.publish(collection, event_properties)
-  #       }.to raise_error(Keen::NotFoundError)
-  #     end
+    describe "failure" do
+      it "should raise a not found error if an invalid project id" do
+        client = Keen::Client.new(:project_id => "riker")
+        expect {
+          client.publish(collection, event_properties)
+        }.to raise_error(Keen::NotFoundError)
+      end
 
-  #     it "should succeed if a non-url-safe event collection is specified" do
-  #       Keen.publish("infinite possibilities", event_properties).should == api_success
-  #     end
-  #   end
+      it "should succeed if a non-url-safe event collection is specified" do
+        Keen.publish("infinite possibilities", event_properties).should == api_success
+      end
+    end
 
-  #   describe "async" do
-  #     # no TLS support in EventMachine on jRuby
-  #     unless defined?(JRUBY_VERSION)
+    describe "async" do
+      # no TLS support in EventMachine on jRuby
+      unless defined?(JRUBY_VERSION)
 
-  #       it "should publish the event and trigger callbacks" do
-  #         EM.run {
-  #           Keen.publish_async(collection, event_properties).callback { |response|
-  #             response.should == api_success
-  #             EM.stop
-  #           }
-  #         }
-  #       end
+        it "should publish the event and trigger callbacks" do
+          EM.run {
+            Keen.publish_async(collection, event_properties).callback { |response|
+              response.should == api_success
+              EM.stop
+            }
+          }
+        end
 
-  #       it "should publish to non-url-safe collections" do
-  #         EM.run {
-  #           Keen.publish_async("foo bar", event_properties).callback { |response|
-  #             response.should == api_success
-  #             EM.stop
-  #           }
-  #         }
-  #       end
-  #     end
-  #   end
-  # end
+        it "should publish to non-url-safe collections" do
+          EM.run {
+            Keen.publish_async("foo bar", event_properties).callback { |response|
+              response.should == api_success
+              EM.stop
+            }
+          }
+        end
+      end
+    end
+  end
 
   describe "queries" do
     let(:api_key) { ENV['KEEN_API_KEY'] }
