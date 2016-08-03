9beb58c0d20513e0e4343f56f8d029b103c309de
Subbu Balakrishnan <subbu@keen.io>
2016-08-03 12:03:53 -0700

    To support compatibility across Ruby versions with default arguments, restructuring of saved_query_response to explicitly accept either a read key or a master key depending on results vs. definition requests

--- lib/keen/client/saved_queries.rb
+++ lib/keen/client/saved_queries.rb
@@ -6,15 +6,18 @@ class SavedQueries
   end
 
   def all
-    process_response(saved_query_response)
+    process_response(saved_query_response(client.master_key))
   end
 
   def get(saved_query_name, results = false)
     saved_query_path = "/#{saved_query_name}"
+    api_key = client.master_key
     if results
       saved_query_path += "/result"
+      # The results path should use the READ KEY
+      api_key = client.read_key
     end
-    response = saved_query_response(saved_query_path, client.read_key)
+    response = saved_query_response(api_key, saved_query_path)
     response_body = JSON.parse(response.body, symbolize_names: true)
     process_response(response)
   end
@@ -41,7 +44,7 @@ class SavedQueries
 
   attr_reader :client
 
-  def saved_query_response(path = "", api_key = client.master_key)
+  def saved_query_response(api_key, path = "")
     Keen::HTTP::Sync.new(client.api_url, client.proxy_url, client.read_timeout).get(
       path: saved_query_base_url + path,
       headers: api_headers(api_key, "sync")
